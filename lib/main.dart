9e13ee9c5b39c6a53505c8cd069f01f07a87afd3
DevZenBook <julien.mangeat@mail-formateur.net>
2023-04-19 11:30:18 +0200

    app se lance et APN s'ouvre

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,12 @@
+import 'package:arcore_flutter_plugin/arcore_flutter_plugin.dart';
 import 'package:flutter/material.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  print('ARCORE IS AVAILABLE?');
+  print(await ArCoreController.checkArCoreAvailability());
+  print('\nAR SERVICES INSTALLED?');
+  print(await ArCoreController.checkIsArCoreInstalled());
   runApp(const MainApp());
 }
 
@@ -10,11 +16,67 @@ class MainApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return const MaterialApp(
-      home: Scaffold(
-        body: Center(
-          child: Text('Hello World!'),
-        ),
-      ),
+      home: Scaffold(body: ARCoreView()),
     );
+  }
+}
+
+class ARCoreView extends StatefulWidget {
+  const ARCoreView({super.key});
+
+  @override
+  _ARCoreViewState createState() => _ARCoreViewState();
+}
+
+class _ARCoreViewState extends State<ARCoreView> {
+  late ArCoreController arCoreController;
+
+  @override
+  void dispose() {
+    arCoreController.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return ArCoreView(
+      onArCoreViewCreated: _onARCoreViewCreated,
+      enableTapRecognizer: true,
+      enableUpdateListener: true,
+    );
+  }
+
+  void _onARCoreViewCreated(ArCoreController controller) {
+    arCoreController = controller;
+    arCoreController.onNodeTap = (node) => _handleNodeTap(node);
+    arCoreController.onPlaneTap = _onPlaneTap;
+  }
+
+  void _handleNodeTap(String node) {
+    print('Node tapped: $node');
+  }
+
+  void _onPlaneTap(List<ArCoreHitTestResult> hits) {
+    final hit = hits.first;
+    print('Plane tapped: ${hit.pose.translation}');
+    _addObject(hit);
+  }
+
+  Future<void> _addObject(ArCoreHitTestResult hit) async {
+    try {
+      final node = ArCoreReferenceNode(
+        name: 'Porche',
+        object3DFileName: 'assets/models/free_animals_-_quirky_series.glb',
+        position: hit.pose.translation,
+        rotation: hit.pose.rotation,
+      );
+
+      print('Adding object: ${node.name} from file: ${node.object3DFileName}');
+
+      arCoreController.addArCoreNodeWithAnchor(node);
+      print('Object added successfully');
+    } catch (e) {
+      print('Error adding object: $e');
+    }
   }
 }
